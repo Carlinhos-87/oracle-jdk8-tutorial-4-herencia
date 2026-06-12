fdb7e4ec2e9eba6a9e94005985836384531cfe19
Carlinhos-87 <jcpapark87@gmail.com>
2026-06-12 20:39:00 +0200

    Antes de explicar la sobreescritura de metodos

--- oracle-jdk8-tutorial-4-herencia/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-4-herencia/src/main/java/com/example/App.java
@@ -1,10 +1,47 @@
 package com.example;
 
-/**
- * Hello world!
- */
+import java.math.BigDecimal;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    /* Ya, en este mismo momento se puede crear una instancia, un objeto, empleado, aunque la clase 
+     * empleado esta vacia.
+     * Pero empleado hereda de persona, y aunque explicitamente no se vea, tanto la clase Persona
+     * como la clase Empleado, tienen un constructor por defecto, sin parametros, que dejara de
+     * tener cuando se cree otro constructor */	
+    	
+    	Empleado empleado1 = new Empleado();
+    	
+    	empleado1.setNombre("Carlos");
+    	empleado1.setPrimerApellido("Mtnez");
+    	
+    	
+    /* Si quisieramos crear un empleado que en el mismo momento de su creacion pudiesemos pasarle
+     * todas las propiedades de la clase Persona*/
+    	
+    	Empleado empleado2 = new Empleado("Elida", "Almonte", "Garcia", Genero.MUJER);
+    	 	
+    	Empleado empleado3 = new Empleado(new BigDecimal(2500.25), Departamento.RRHH);
+    	
+    	/* Ejercicio # del Viernes 12 de Junio
+    	* 
+    	* Al empleado3 agregarle como nombre "Miguel Angel", genero HOMBRE. Mostrar
+    	* por la consola la informacion del empleado 3 */
+    	
+    	empleado3.setNombre("Miguel Angel");
+    	empleado3.setGenero(Genero.HOMBRE);
+    	
+    	/*System.out.println("Nombre del empleado: " + empleado3.getNombre() 
+    	+ " , con Genero: " + empleado3.getGenero() + ", un Salario de: " 
+    	+ empleado3.getSalario() + ", del Departamento: " + empleado3.getDepartamento());*/
+    	
+    	System.out.println(empleado3.toString());
+    	
+    	Empleado empleado4 = new Empleado("Carlos", "Paino", "Agueda", Genero.HOMBRE, new BigDecimal(2500), Departamento.INFORMATICA);
+    	
+    	
     }
+    	
+    	
 }
--- oracle-jdk8-tutorial-4-herencia/src/main/java/com/example/Empleado.java
+++ oracle-jdk8-tutorial-4-herencia/src/main/java/com/example/Empleado.java
@@ -7,15 +7,74 @@
  * que son publicos de otra clase, excepto los constructores que no
  * se heredan
  * 
- * Todas las clases heredan de la clase Object, pero una clase solamente puede
- * heredar de otra, no de mas de una a la vez, porque Java, a diferencia de
- * lenguajes como C++ no permite la herencia multiple de clases.
+ * Todas las clases heredan de la clase Object, pero una clase solamente
+ * puede heredar de otra, no de mas de una a la vez, porque Java, a 
+ * diferencia de lenguajes como C++ no permite la herencia multiple de 
+ * clases.
  * 
- * Y como en la naturaleza una clase puede heredar propiedades y metodos de 
- * sus ancentros y puede mejorar alguno de los elementos que ha heredado */
+ * Y como en la naturaleza una clase puede heredar propiedades y metodos 
+ * de sus ancestros y puede mejorar alguno de los elementos que ha 
+ * heredado */
 
 package com.example;
 
+import java.math.BigDecimal;
+
 public class Empleado extends Persona {
 
+	private BigDecimal salario;
+	private Departamento departamento;
+	
+	public Empleado() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Empleado(String nombre, String primerApellido, String segundoApellido, Genero genero) {
+		super(nombre, primerApellido, segundoApellido, genero);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Empleado(BigDecimal salario, Departamento departamento) {
+		super();
+		this.salario = salario;
+		this.departamento = departamento;
+	}
+	
+	/*Generar un constructor que permita instanciar un empleado con los campos de Persona
+	 * y los propios de Empleado*/
+	
+	public Empleado(String nombre, String primerApellido, String segundoApellido, Genero genero, BigDecimal salario,
+			Departamento departamento) {
+		super(nombre, primerApellido, segundoApellido, genero);
+		this.salario = salario;
+		this.departamento = departamento;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [salario=" + salario + ", departamento=" + departamento + ", getNombre()=" + getNombre()
+				+ ", getPrimerApellido()=" + getPrimerApellido() + ", getSegundoApellido()=" + getSegundoApellido()
+				+ ", getGenero()=" + getGenero() + "]";
+	}
+
+	
+	
 }
